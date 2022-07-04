63acbde226f02350386df2efd4d4437bb1ca6d24
Axole Maranjana <axolemaranjana4@gmail.com>
2022-07-05 01:42:41 +0200

    Added the hero section

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import heroImg from "../assets/Group 77.png";
+
+const Hero = () => {
+  return (
+    <section className="hero-section">
+      <img className="hero-image" src={heroImg} alt="Hero" />
+      <h1 className="hero-title">Online Experiences</h1>
+      <p className="hero-description">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </section>
+  );
+};
+export default Hero;
